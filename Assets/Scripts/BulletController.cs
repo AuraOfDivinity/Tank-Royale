2c4ea55e6424d6df937ad806e292b6daed286e93
Asel <peirisasel373@gmail.com>
2019-11-30 17:14:49 +0530

    Bullet explosion

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class BulletController : MonoBehaviourPun
+{
+    public GameObject hitEffect;
+    PhotonView pv;
+    Rigidbody2D rb;
+    Transform tf;
+    public float bulletForce = 30f;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Triggered");
+        Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        pv = GetComponent<PhotonView>();
+        rb = GetComponent<Rigidbody2D>();
+        tf = GetComponent<Transform>();
+        rb.AddForce(tf.up * bulletForce,ForceMode2D.Impulse);
+        //Destroy(gameObject, 5f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Instantiate(hitEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
